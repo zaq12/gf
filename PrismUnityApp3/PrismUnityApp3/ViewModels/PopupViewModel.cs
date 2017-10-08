be783c81124207218353060152aa1ef2c58aa351
Paweł Gwizdalski <pawel.gwizdalski@hotmail.com>
2017-10-08 21:10:50 +0200

    Custom Picker

--- PrismUnityApp3/PrismUnityApp3/ViewModels/PopupViewModel.cs
+++ PrismUnityApp3/PrismUnityApp3/ViewModels/PopupViewModel.cs
@@ -5,17 +5,19 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace PrismUnityApp3.ViewModels
 {
-    public class PopupViewModel : BindableBase
+    public class PopupViewModel : MainPageViewModel
     {
 
-        public PopupViewModel()
+        public PopupViewModel(INavigationService navigationService) : base(navigationService)
         {
 ;
-            ListElements = new ObservableCollection<string> {"b"};
-
+            ListElements = new ObservableCollection<string> { "b", "a", "c" };
+            GoToSelectedItemCommand = new Command<string>(GoToSelectedItem);
         }
 
         private ObservableCollection<string> _listElements;
@@ -24,6 +26,15 @@ namespace PrismUnityApp3.ViewModels
         {
             get { return _listElements; }
             set { SetProperty(ref _listElements, value); }
+        }
+
+        public ICommand GoToSelectedItemCommand { get; set; }
+        protected async void GoToSelectedItem(string value)
+        {
+            var p = new NavigationParameters();
+            p.Add("contact", value);
+            await NavigationService.PopupGoBackAsync(p);
+            // this.Title = value;
         }
     }
 }
